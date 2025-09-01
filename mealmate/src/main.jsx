6768ccb3d232a0c04c373f4a0f1891e90b24f090
Cynthia Sosa <cynthiasosa33@iclould.com>
2025-09-01 08:06:38 -0400

    "prepare for deploy"

--- mealmate/src/main.jsx
+++ mealmate/src/main.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Recipes from "./pages/Recipes.jsx";
+import Favorites from "./pages/Favorites.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
 );
